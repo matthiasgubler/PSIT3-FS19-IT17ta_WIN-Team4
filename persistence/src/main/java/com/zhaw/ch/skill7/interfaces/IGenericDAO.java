22ba6c1ae0dda5059f682f77eb63f10133c69f17
Matthias <matthiasgubler@me.com>
2019-04-28 14:48:30 +0200

    added some JavaDoc

--- persistence/src/main/java/com/zhaw/ch/skill7/interfaces/IGenericDAO.java
+++ persistence/src/main/java/com/zhaw/ch/skill7/interfaces/IGenericDAO.java
@@ -5,14 +5,38 @@ import java.util.Optional;
 
 public interface IGenericDAO<T> {
 
+    /**
+     * Liest eine Liste aller Objekte T aus der Datenbank
+     *
+     * @return
+     */
     List<T> read();
 
+    /**
+     * Liest ein einzelnes Objekt T von der Datenbank anhand deren ID
+     *
+     * @param id ID des zu landenden Objekts
+     * @return Optional des gesuchten Objekt. Dieses beinhaltet das gesuchte Objekt oder Optional.empty(), falls das Objekt anhand der ID nicht gefunden wurde in der Datenbank
+     */
     Optional<T> byId(long id);
 
+    /**
+     * Fügt ein Objekt T der Datenbank hinzu
+     *
+     * @param object Objekt, dass in die Datenbank gespeichert wird
+     */
     void add(T object);
 
+    /**
+     * Löscht ein Objekt T aus der Datenbank
+     * @param object Objekt, dass gelöscht werden soll
+     */
     void remove(T object);
 
+    /**
+     * Führt einen Update auf ein Objekt T in der Datenbank durch
+     * @param object Objekt, welches updated werden soll
+     */
     void update(T object);
 
 }
--- persistence/src/main/java/com/zhaw/ch/skill7/model/Updateable.java
+++ persistence/src/main/java/com/zhaw/ch/skill7/model/Updateable.java
@@ -1,5 +1,15 @@
 package com.zhaw.ch.skill7.model;
 
+/**
+ * Interface eines Objekt welches Updateable / Updatebar ist
+ *
+ * @param <T> Typ des updatbaren Objekt
+ */
 public interface Updateable<T> {
+
+    /**
+     * Führt eine Update auf ein Objekt durch durch ein weiteres Objekt
+     * @param objectWithNewData Objekt mit den neuen Daten, die auf das Updateable Objekt übernommen werden
+     */
     void update(T objectWithNewData);
 }
