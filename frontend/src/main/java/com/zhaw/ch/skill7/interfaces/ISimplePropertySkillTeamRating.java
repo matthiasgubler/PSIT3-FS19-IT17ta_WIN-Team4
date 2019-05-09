2ad6e1d4493fd80b5392fa935fb04ad8d215a232
ojtanner <oliver-tanner@outlook.com>
2019-05-09 10:46:33 +0200

    Clean up - remove unused imports

--- frontend/src/main/java/com/zhaw/ch/skill7/interfaces/ISimplePropertySkillTeamRating.java
+++ frontend/src/main/java/com/zhaw/ch/skill7/interfaces/ISimplePropertySkillTeamRating.java
@@ -1,9 +1,5 @@
 package com.zhaw.ch.skill7.interfaces;
 
-import com.zhaw.ch.skill7.domain.model.SkillTeamRating;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-
 public interface ISimplePropertySkillTeamRating {
 
     public String getSkill();
