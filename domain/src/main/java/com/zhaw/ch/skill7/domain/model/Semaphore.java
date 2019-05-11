9a3317ba6a3dae4154fc211394a941d6e75c0959
ojtanner <oliver-tanner@outlook.com>
2019-05-11 17:25:36 +0200

    Add JavaDoc to Semaphore-enum and individual enums.

--- domain/src/main/java/com/zhaw/ch/skill7/domain/model/Semaphore.java
+++ domain/src/main/java/com/zhaw/ch/skill7/domain/model/Semaphore.java
@@ -1,8 +1,23 @@
 package com.zhaw.ch.skill7.domain.model;
 
+/**
+ * Enum für die Bewertung der Teams und deren SkillTeamRatings
+ */
 public enum Semaphore {
+    /**
+     * Optimale Bewertung
+     */
     GREEN,
+    /**
+     * Ausreichende Bewertung
+     */
     YELLOW,
+    /**
+     * Ungenügende Bewertung
+     */
     RED,
+    /**
+     * Keine Bewertung möglich
+     */
     WHITE
 }
